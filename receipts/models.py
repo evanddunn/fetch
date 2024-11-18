23989772fe59e13858b8315ec7cfe44d0c89466b
Evan Dunn <35546160+evanddunn@users.noreply.github.com>
2024-11-18 16:24:00 -0500

    quick model to make it all work

--- receipts/models.py
+++ receipts/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Receipt(models.Model):
+    uuid = models.UUIDField(
+        _("uuid"), default=uuid.uuid4, editable=False, db_index=True
+    )
+    data = models.JSONField(
+        _("data"), null=False, blank=False
+    )  # Since we aren't really doing DB stuff, not going to break this out into its parts just store it together
